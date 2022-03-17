993983138c4eb90268e5477090c184b1930fe464
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-03-17 15:04:06 -0700

    Add(components): Created components for app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//components
+import Wrapper from './Components/Wrapper/wrapper.js';
+import Screen from './Components/Screen/screen.js';
+
+function App() {
+  return (
+    <div>
+      <Wrapper>
+        <Screen value='0' />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
